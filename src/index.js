1705cc6a76b477eedaecc0e5a96b0008a7f8a634
Tolik Dem <tolik.demianov@gmail.com>
2018-06-25 15:38:15 +0300

    создал Store и Reduser для использования в этом приложении

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './normalize.css';
 import './index.css';
 import App from './App';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <App />,
