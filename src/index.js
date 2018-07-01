b9812ee72b0addaa4ce05710e759bf52390d8b4a
Tolik Dem <tolik.demianov@gmail.com>
2018-07-01 19:11:00 +0300

    просто коммит

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import {
   createStore, combineReducers, applyMiddleware, compose,
 } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import './normalize.css';
 import './index.css';
 import App from './App';
@@ -26,7 +27,16 @@ A middleware can inspect actions and state, modify actions, dispatch other actio
 stop actions from reaching the reducers, and much more.
 
 Middleware are also a good place for managing persistent server connections via websockets,
-and other similar behavior.
+and other similar behavior. https://blog.isquaredsoftware.com/presentations/2018-03-redux-fundamentals/#/23
+
+Middleware provides a third-party extension point between dispatching an action, and the moment it
+reaches the reducer.
+People use Redux middleware for logging, crash reporting, talking to an asynchronous API, routing,
+and more. https://redux.js.org/advanced/middleware
+
+Middleware is created by the community and does not ship with Redux by default.
+You need to explicitly install packages like redux-thunk or redux-promise to use it.
+You may also create your own middleware. https://redux.js.org/api-reference/store#notes
 */
 const logger = store => next => (action) => {
   console.log('[Middleware] Dispatching', 'action: ', action);
@@ -41,13 +51,15 @@ const logger = store => next => (action) => {
 
 // соединяем наш middleware с нашим store
 // в функцию applyMiddleware можно добавлять столько middleware, скольно нужно через запятую
-// applyMiddleware(looger, logger2, logger3)
+// applyMiddleware(looger, thunk)
+// looger это middleware которое мы тут сами написали
+// thunk это готовый пакет middleware https://github.com/reduxjs/redux-thunk
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
