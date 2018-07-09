69e23530fa2ee4157bac57b31a29cd2b10c37c8d
Tolik Dem <tolik.demianov@gmail.com>
2018-07-09 19:13:05 +0300

    добавил еще коммент

--- src/store/redusers/result.js
+++ src/store/redusers/result.js
@@ -9,6 +9,10 @@ const reducer = (state = initialState, action) => {
   const newState = _.cloneDeep(state);
   switch (action.type) {
     case actionTypes.STORE_RESULT:
+      // Внутри редюсера тоже можно реализовывать какую-либо логику, менять, обрабатывать данные
+      // и потом уже возвращать newState. Но эта операция, я так понимаю всегда синхронна
+      // реализовывать логику над данными получаемыми асинхронно следует в middleware.
+      // Например в action creators c использованием middleware от redux-thunk
       newState.results = newState.results.concat({ id: `${new Date()} ${Math.random()}`, value: action.value });
       return newState;
     case actionTypes.DELETE_RESULT:
