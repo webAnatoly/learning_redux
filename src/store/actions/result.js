9f8a5cf91b0d4a9d7d01bc44329e8285baf0ebb8
Tolik Dem <tolik.demianov@gmail.com>
2018-07-09 18:46:55 +0300

    added comment

--- src/store/actions/result.js
+++ src/store/actions/result.js
@@ -7,6 +7,10 @@ export const saveResult = payload => (
   }
 );
 
+/*
+The only place where we can execute asynchronous code is in our action creator.
+It's what redux-funk is made for and it's the common and best practice pattern.
+*/
 export const storeResult = payload => dispatch => (
   setTimeout(() => {
     dispatch(saveResult(payload));
